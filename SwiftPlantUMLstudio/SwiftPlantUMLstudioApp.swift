48c2dc424a3a611027f0c3baec86a877399812d2
Joseph D Cursio <josephcursio@icloud.com>
2026-02-26 07:29:43 -0600

    Initial Commit

---
+++ SwiftPlantUMLstudio/SwiftPlantUMLstudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPlantUMLstudioApp.swift
+//  SwiftPlantUMLstudio
+//
+//  Created by joe cursio on 2/26/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPlantUMLstudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftPlantUMLstudioTests/SwiftPlantUMLstudioTests.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftPlantUMLstudioTests.swift
+//  SwiftPlantUMLstudioTests
+//
+//  Created by joe cursio on 2/26/26.
+//
+
+import Testing
+@testable import SwiftPlantUMLstudio
+
+struct SwiftPlantUMLstudioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
